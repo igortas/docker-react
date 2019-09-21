0244024ad40d80c18486ee7ae4be04fac9d9d77e
Igor Tashevski <igor-t@live.com>
2019-09-22 00:49:57 +0200

    accept current change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -22,33 +21,6 @@ function App() {
       </header>
     </div>
   );
-=======
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           rrrrr Docker!!!!!!!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            I was changed on feature branch
-          </a>
-        </header>
-      </div>
-    );
-  }
->>>>>>> 126ebec49701c498860ca2311b2d9e7b8541939f
 }
 
 export default App;
